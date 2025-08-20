80e79adb12dcefb06dc8594ec7eb771be2da6df6
nahid2887 <nahid2887@gmail.com>
2025-08-20 14:52:48 +0600

    updated

--- claim/settings.py
+++ claim/settings.py
@@ -69,22 +69,23 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://10.10.13.27:9000",
-    "http://10.10.13.31:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:5173",  # Added for your frontend
-    "http://localhost:5179",
-    "http://10.10.13.32:5179",
-    "https://win-a-claim-ai.netlify.app",
-    "https://front.winaclaim.com"
+# CORS_ALLOWED_ORIGINS = [
+#     "http://10.10.13.27:9000",
+#     "http://10.10.13.31:5173",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://localhost:5173",  # Added for your frontend
+#     "http://localhost:5179",
+#     "http://10.10.13.32:5179",
+#     "https://win-a-claim-ai.netlify.app",
+#     "https://front.winaclaim.com",
+#     "https://charbel.72-60-30-162.sslip.io"
     
-]
+# ]
 
 CSRF_TRUSTED_ORIGINS = ["https://72.60.30.162", "https://api.winaclaim.com"]
 
-CORS_ALLOW_ALL_ORIGINS = False 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_HEADERS = [
     'accept',
