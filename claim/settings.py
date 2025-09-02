6bcf87030b2c2f295438e834ec7c88317966236a
nahid2887 <nahid2887@gmail.com>
2025-09-02 12:03:13 +0600

    feat: update email backend settings to use environment variables for configuration

--- claim/settings.py
+++ claim/settings.py
@@ -215,9 +215,9 @@ SIMPLE_JWT = {
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ['true', '1', 'yes']
 
 
 # Replace these with your own credentials
